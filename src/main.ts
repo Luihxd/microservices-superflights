4a0854200bfc3ba1986bd76361a295e8649eb2d1
Luis ChM <83924924+Luihxd@users.noreply.github.com>
2024-05-26 23:46:05 -0600

    Swagger configured

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,25 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionFilter());
   //Global interceptors
   app.useGlobalInterceptors(new TimeOutInterceptor());
+
+  const options = new DocumentBuilder()
+  .setTitle("Superflight API v2")
+  .setDescription("Scheduled flights Microservices")
+  .setVersion("2.0.0")
+  ///.addBearerAuth()
+  .build();
+
+  app.enableCors();
+
+  const document = SwaggerModule.createDocument(app, options);
+  
+
+  SwaggerModule.setup('/api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
