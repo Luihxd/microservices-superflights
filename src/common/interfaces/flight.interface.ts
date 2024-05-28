a0e99db96c0e9f6fbe06517a5fbaecb659325632
Luis ChM <83924924+Luihxd@users.noreply.github.com>
2024-05-27 23:39:16 -0600

    restructuration of api-gateway

--- src/common/interfaces/flight.interface.ts
+++
@@ -1,9 +0,0 @@
-import { IPassenger } from "./passenger.interface";
-
-export interface IFlight{
-    pilot: string;
-    airplane: string;
-    destinationCity: string;
-    flightDate: Date;
-    passengers: IPassenger[];
-}
--- src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { ProxyModule } from 'src/common/proxy/proxy.module';
-
-@Module({
-  imports: [ProxyModule],
-  controllers: [UsersController],
-})
-export class UsersModule {}
