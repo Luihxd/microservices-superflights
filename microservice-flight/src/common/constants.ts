99183fbb04b54f01965eb6437eea315c6b767b32
Luis ChM <83924924+Luihxd@users.noreply.github.com>
2024-06-03 22:06:24 -0600

    microservice-flight rabbitmq and mongoose connect

---
+++ microservice-flight/src/common/constants.ts
@@ -0,0 +1,15 @@
+export enum RabbitMQ {
+    FlightQueue = 'flights'
+
+}
+
+export enum FlightMSG {
+    CREATE = 'CREATE_FLIGHT',
+    FIND_ALL = 'FIND_ALL_FLIGHTS',
+    FIND_ONE = 'FIND_ONE_FLIGHT',
+    UPDATE = 'UPDATE_FLIGHT',
+    DELETE = 'DELETE_FLIGHT',
+    ADD_PASSENGER = 'ADD_PASSENGER'
+}
+
+
